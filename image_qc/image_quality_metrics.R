2fbf70a83f382f0ce4fd8e8173ca3d3c330272a2
Maria Keays <maria.keays@gmail.com>
2019-05-22 15:45:54 +0100

    Adding comments to R code

--- image_qc/image_quality_metrics.R
+++ image_qc/image_quality_metrics.R
@@ -1,18 +1,29 @@
 
-fourierBasedSharpnessMetric <- function( fftshiftFunctionsFile, plane ) {
+# Algorithms for calculating image quality metrics.
 
+# This algorithm is defined by De and Masilamani, 2013
+# (https://doi.org/10.1016/j.proeng.2013.09.086).
+fourierBasedSharpnessMetric <- function( fftshiftFunctionsFile, plane ) {
+    
+    # Load functions for shifting origin of FT representation to centre.
     source( fftshiftFunctionsFile )
     
+    # Calculate Fourier Transform representation of image plane.
     fftimage <- fft( plane )
 
+    # Shift the origin of the FT representation to centre.
     shifted <- fftshift( fftimage )
 
+    # Calculate the absolute values of the centred FT.
     shiftedAbs <- abs( shifted )
-
+    
+    # Find M, the maximum value of the frequency component in the FT representation.
     M <- max( shiftedAbs )
 
+    # Calculate the total number of pixels whose value is greater than M/1000.
     Th <- sum( shiftedAbs > ( M/1000 ) )
 
+    # Calculate the image quality measure (sharpness), Th divided by the image area.
     sharpness <- Th/prod( dim( plane ) )
 
     return( sharpness )
