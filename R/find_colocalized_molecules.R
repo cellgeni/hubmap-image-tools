92dc5695324398b86536ead239cfcf8a12a134f7
Maria Keays <maria.keays@gmail.com>
2019-08-07 10:14:03 +0100

    adding script to find colocalized molecules

---
+++ R/find_colocalized_molecules.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+# Source required functions.
+functionsFile <- file.path(
+    Sys.getenv( "HUBMAP_GIT" ),
+    "R",
+    "image_data_functions.R"
+)
+source( functionsFile )
+
+# Parse cmdline args
+args <- commandArgs( TRUE )
+
+# File containing all spectral intensities and coordinates in CSV format.
+massSpecDataFile <- args[ 1 ]
+
+# File containing metadata describing molecules represented in the data.
+molecularMetadataFile <- args[ 2 ]
+
+# Molecule to search with, as "Elemental composition" e.g. "C39H79N2O6P".
+molecule <- args[ 3 ]
+
+# Read in mass spec data files and build object.
+msdata <- create_msimagingexperiment_object( massSpecDataFile, molecularMetadataFile )
+
+colocalizedFeatures <- find_colocalized_features( msdata, molecule )
+
+print( colocalizedFeatures )
