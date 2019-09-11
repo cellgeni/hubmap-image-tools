814e43e767c1fb5c862bdaa04621c863dac4f464
Maria Keays <maria.keays@gmail.com>
2019-09-11 16:25:44 +0100

    added logging to seqFISH cript

--- python/starfish/seqfish.py
+++ python/starfish/seqfish.py
@@ -3,6 +3,7 @@
 # Adapted from https://github.com/spacetx/starfish/blob/master/notebooks/SeqFISH.ipynb
 
 import argparse
+import logging
 
 import os
 from copy import deepcopy
@@ -22,9 +23,20 @@ ap.add_argument(
 )
 args = vars( ap.parse_args() )
 
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(levelname)s - %(message)s'
+)
+
+logging.info( "Output file name " + args[ "outfile" ] )
+
+logging.info( "Loading test data from cloud..." )
+
 # Select seqFISH data for a single field of view.
 exp = starfish.data.SeqFISH( use_test_data=True )
 img = exp[ 'fov_000' ].get_image( 'primary' )
+
+logging.info( "Test data loaded." )
 
 # The first step in seqFISH is to do some rough registration. For this example
 # data, the rough registration has already been done by the authors, so this
@@ -34,14 +46,20 @@ img = exp[ 'fov_000' ].get_image( 'primary' )
 # Remove image background, using a White Tophat filter, which measures the
 # background with a folling disck morphological element and subtracts it from
 # the image.
+logging.info( "Running background correction..." )
+
 wth = starfish.image.Filter.WhiteTophat( masking_radius = 3 )
 background_corrected = wth.run( img, in_place = False )
+
+logging.info( "Background correction complete." )
 
 # Scale images to equalize spot intensities across channels.
 # The number of peaks are not uniform across rounds and channels, which
 # prevents histogram matching across channels. Instead, a percentile value is
 # identified and set as the maximum across channels, and the dynamic range is
-# extended to equalize the channel intensities
+# extended to equalize the channel intensities.
+logging.info( "Scaling images to equalize spot intensities..." )
+
 clip = starfish.image.Filter.Clip( 
     p_max = 99.9, 
     expand_dynamic_range = True, 
@@ -49,18 +67,27 @@ clip = starfish.image.Filter.Clip(
 )
 scaled = clip.run( background_corrected, in_place = False )
 
+logging.info( "Scaling complete." )
+
 # Remove residual background.
 # The background is fairly uniformly present below intensity=0.5. However,
 # starfish's clip method only supports percentiles. To solve this problem, the
 # intensities can be directly edited in the underlying numpy array.
+logging.info( "Removing residual backgroun from images..." )
+
 clipped = deepcopy( scaled )
 clipped.xarray.values[ clipped.xarray.values < 0.7 ] = 0
+
+logging.info( "Residual background removed." )
 
 # In the example in the notebook, they first try using a local blob detector to
 # detect the spots. However, they report that based on visual inspection of
 # results using a local blob detector, it looks like the spot correspondence
 # across rounds is not being detected well. 
 # So instead, they then try using the PixelSpotDetector.
+
+logging.info( "Detecting pixels using PixelSpotDetector and Gaussian filtering..." )
+
 glp = starfish.image.Filter.GaussianLowPass( sigma = ( 0.3, 1, 1 ), is_volume=True )
 blurred = glp.run( clipped )
 psd = starfish.spots.DetectPixels.PixelSpotDecoder(
@@ -72,6 +99,8 @@ psd = starfish.spots.DetectPixels.PixelSpotDecoder(
     max_area = 50,
 )
 pixel_decoded, ccdr = psd.run( blurred )
+
+logging.info( "Pixel detection complete." )
 
 # Create a data frame of the spot intensities.
 spots_df = pixel_decoded.to_features_dataframe()
